49bf41586f8606e98101bbe030d31d2579d23c01
Ardjan Dommisse <ardjan@gmail.com>
2016-08-02 13:16:48 +0200

    Exercise 7.1

---
+++ exercises/src/test/scala/fpinscala/parallelism/ParSpec.scala
@@ -0,0 +1,15 @@
+package fpinscala.parallelism
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  * Created by Ardjan on 13/06/2016.
+  */
+class ParSpec extends FlatSpec with Matchers {
+  "Exercise 7.1" should "be specified" in {
+    /*
+    Par.map2[A,B,C](l: Par[A], r: Par[B])(f: (A,B) => C): Par[C]
+
+     */
+  }
+}
