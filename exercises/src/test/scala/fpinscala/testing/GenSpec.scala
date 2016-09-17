de676f5f3849077ae0db67b6641222208be8cc8c
Ardjan Dommisse <ardjan@gmail.com>
2016-09-17 10:47:06 +0200

    Exercise 8.2

--- exercises/src/test/scala/fpinscala/testing/GenSpec.scala
+++ exercises/src/test/scala/fpinscala/testing/GenSpec.scala
@@ -16,7 +16,32 @@ class GenSpec extends FlatSpec with Matchers {
   "Exercise 8.1 sum of same values" should "be equal to the length of the list times that value" in {
     val value = Gen.unit[Int](10)
 
-    val prop = Prop.forAll(value)((v) => sum(List.fill(100)(v)) == 100*v)
+    val prop = Prop.forAll(value)( v => sum(List.fill(100)(v)) == 100*v)
+
+    prop.check
+  }
+
+  def max(l: List[Int]): Int = l.max
+  "Exercise 8.2 max of a list" should "be greater than any value of that list" in {
+    val intList = Gen.listOf(Gen.choose(0,100))
+
+    val prop = Prop.forAll(intList)(ns => max(ns) == ns.max)
+
+    prop.check
+  }
+
+  "Exercise 8.2 max of a list of one item" should "be that item" in {
+    val value = Gen.unit[Int](10)
+
+    val prop = Prop.forAll(value)( v => max(List(v))  == v )
+
+    prop.check
+  }
+
+  "Exercise 8.2 max of a list of items with the same value" should "be that item" in {
+    val value = Gen.unit(10)
+
+    val prop = Prop.forAll(value)( v => max(List.fill(100)(v)) == v )
 
     prop.check
   }
