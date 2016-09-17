06296fbc3c9576e45705360707e110060a0faec7
Ardjan Dommisse <ardjan@gmail.com>
2016-09-17 10:30:55 +0200

    Exercise 8.1 iso. listing.

--- exercises/src/test/scala/fpinscala/testing/GenSpec.scala
+++ exercises/src/test/scala/fpinscala/testing/GenSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.{FlatSpec, Matchers}
 class GenSpec extends FlatSpec with Matchers {
   def sum(l: List[Int]): Int = l.sum
 
-  "Listing 8.1 sum reversed" should "be equal to the regular sum" in {
+  "Exercise 8.1 sum reversed" should "be equal to the regular sum" in {
     val intList = Gen.listOf(Gen.choose(0, 100))
 
     val prop = Prop.forAll(intList)(ns => sum(ns.reverse) == sum(ns))
@@ -13,7 +13,7 @@ class GenSpec extends FlatSpec with Matchers {
     prop.check
   }
 
-  "Listing 8.1 sum of same values" should "be equal to the length of the list times that value" in {
+  "Exercise 8.1 sum of same values" should "be equal to the length of the list times that value" in {
     val value = Gen.unit[Int](10)
 
     val prop = Prop.forAll(value)((v) => sum(List.fill(100)(v)) == 100*v)
