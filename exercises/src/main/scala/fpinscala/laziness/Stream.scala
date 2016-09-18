1a13f81333d37b447abd7d74f33b719c5af0a95d
Ardjan Dommisse <ardjan@gmail.com>
2016-09-18 19:53:20 +0200

    Exercise 8.9

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -91,6 +91,10 @@ trait Stream[+A] {
       case _ => None
     }
 
+  // special case of `zipWith`
+  def zip[B](s2: Stream[B]): Stream[(A,B)] =
+  zipWith(s2)((_,_))
+
   def zipAll[B](s2: Stream[B]): Stream[(Option[A], Option[B])] =
     unfold((this, s2)) {
       case (Cons(hl, tl), Cons(hr, tr)) => Some((Some(hl()), Some(hr())), (tl(), tr()))
--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -2,35 +2,76 @@ package fpinscala.testing
 
 import fpinscala.state.State
 import fpinscala.state.RNG
-import fpinscala.testing.Prop.{FailedCase, Result, SuccessCount, TestCases}
+import fpinscala.laziness.Stream
+import fpinscala.testing.Prop.{FailedCase, SuccessCount, TestCases}
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
 
-case class Prop(run: TestCases => Result) {
+sealed trait Result {
+  def isFalsified: Boolean
+}
+
+case object Passed extends Result {
+  def isFalsified = false
+}
+
+case class Falsified(failure: FailedCase, successes: SuccessCount) extends Result {
+  def isFalsified: Boolean = true
+}
+
+case class Prop(run: (TestCases, RNG) => Result) {
   def check: Either[(FailedCase, SuccessCount), SuccessCount] = ???
 
-  def &&(that: Prop): Prop = new Prop (
-    (tc: TestCases) =>
-      (Prop.this.check, that.check) match {
-        case (Right(l), Right(r)) => Right(l + r)
-        case (Right(l), Left((f, r))) => Left((f, l + r))
-        case (Left((f, l)), Right(r)) => Left((f, l + r))
-        case (Left((fl, l)), Left((fr, r))) => Left((fl + fr, l + r))
+  def &&(that: Prop): Prop = new Prop(
+    (tc: TestCases, rng) => Prop.this.run(tc, rng) match {
+        case Passed => that.run(tc,rng)
+        case f => f
+      }
+  )
+
+  def ||(that: Prop): Prop = new Prop(
+    (tc: TestCases, rng) =>
+      Prop.this.run(tc, rng) match {
+        case f:Falsified => that.run(tc, rng)
+        case x => x
+      }
+  )
+
+  def tag(tag: String): Prop = new Prop(
+    (tc, rng) =>
+      Prop.this.run(tc, rng) match {
+        case f: Falsified => Falsified(s"${tag} ${f.failure}", f.successes)
+        case x => x
       }
   )
 }
 
 object Prop {
   type TestCases = Int
-  type Result = Either[(FailedCase, SuccessCount), SuccessCount]
 
   type FailedCase = String
   type SuccessCount = Int
 
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+  def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n, rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch {
+        case e: Exception => Falsified(buildMsg(a, e), i)
+      }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
 }
 
 
@@ -48,22 +89,22 @@ object Gen {
   def choose(lb: Int, ub: Int): Gen[Int] =
     Gen(State(RNG.nonNegativeInt).map((v: Int) => lb + v % (ub - lb)))
 
-  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap( b => if(b) g1 else g2 )
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = boolean.flatMap(b => if (b) g1 else g2)
 
-  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A],Double)): Gen[A] = {
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
     val threshold = g1._2.abs / (g1._2.abs + g2._2.abs)
     double.flatMap(d => if (d < threshold) g1._1 else g2._1)
   }
 }
 
 case class Gen[A](sample: State[RNG, A]) {
-  def map[B](f: A => B): Gen[B] = Gen(this.sample.map( a => f(a)))
+  def map[B](f: A => B): Gen[B] = Gen(this.sample.map(a => f(a)))
 
-  def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap( a => f(a).sample))
+  def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
 
   def listOfN(n: Int): Gen[List[A]] = Gen.listOfN(n, this)
 
-  def listOfN(size: Gen[Int]): Gen[List[A]] = size flatMap( s => this.listOfN(s))
+  def listOfN(size: Gen[Int]): Gen[List[A]] = size flatMap (s => this.listOfN(s))
 }
 
 trait SGen[+A] {
