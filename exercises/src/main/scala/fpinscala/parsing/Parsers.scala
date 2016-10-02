6eaae88db235564d5c35ae5aee35702f2232c85a
Ardjan Dommisse <ardjan@gmail.com>
2016-10-02 12:06:59 +0200

    Chapter 9 Exercise 9.1

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -33,6 +33,12 @@ trait Parsers[ParseError, Parser[+ _]] {
 
   def product[A, B](p: Parser[A], p2: Parser[B]): Parser[(A, B)]
 
+  def map2[A,B,C](p: Parser[A], p2: Parser[B])(f: (A,B) => C) : Parser[C] =
+    map(product(p,p2))(f.tupled)
+
+  def many1[A](p: Parser[A]): Parser[List[A]] =
+    map2(p, many(p))( _ :: _ )
+
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
 
