61f65544bb095d443275fc0810935f5a5d2eace0
Ardjan Dommisse <ardjan@gmail.com>
2016-10-03 23:40:13 +0200

    Chapter 9.5

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -36,10 +36,10 @@ trait Parsers[ParseError, Parser[+ _]] {
 
   def slice[A](p: Parser[A]): Parser[String]
 
-  def product[A, B](p: Parser[A], p2: Parser[B]): Parser[(A, B)]
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)]
 
-  def map2[A, B, C](p: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] =
-    map(product(p, p2))(f.tupled)
+  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
+    product(p, p2) map f.tupled
 
   def many1[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(_ :: _)
