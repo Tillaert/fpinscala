6e2b60bfdc05d1b344666440b438edebcacd0be6
Tillaert <ardjan@gmail.com>
2016-10-12 21:44:45 +0200

    Some reordering... didn't help

--- exercises/src/main/scala/fpinscala/parsing/JSON.scala
+++ exercises/src/main/scala/fpinscala/parsing/JSON.scala
@@ -1,25 +1,26 @@
 package fpinscala.parsing
 
 import language.higherKinds
+import language.implicitConversions
 
 trait JSON
 
 object JSON {
-  def jsonParser[Parser[+ _]](P: Parsers[Parser]): Parser[JSON] = {
+  case object JNull extends JSON
+  case class JNumber(get: Double) extends JSON
+  case class JString(get: String) extends JSON
+  case class JBool(get: Boolean) extends JSON
+  case class JArray(get: IndexedSeq[JSON]) extends JSON
+  case class JObject(get: Map[String, JSON]) extends JSON
+
+  def jsonParser[Parser[+_]](P: Parsers[Parser]): Parser[JSON] = {
     import P._
 
-    case object JNull extends JSON
-    case class JNumber(get: Double) extends JSON
-    case class JString(get: String) extends JSON
-    case class JBool(get: Boolean) extends JSON
-    case class JArray(get: IndexedSeq[JSON]) extends JSON
-    case class JObject(get: Map[String, JSON]) extends JSON
+   // val spaces = char(' ').many.slice
 
-    val spaces = char(' ').many.slice
-
-    def obj: Parser[JSON] = surround("{", "}")(keyval sep "," map (kvs => JObject(kvs.toMap)))
     def array: Parser[JSON] = surround("[", "]")(value sep "," map (vs => JArray(vs.toIndexedSeq)))
-    def keyval = escapedQuoted ** (":" *> obj)
+    def obj: Parser[JSON] = surround("{", "}")(keyval sep "," map (kvs => JObject(kvs.toMap)))
+    def keyval = escapedQuoted ** (":" *> value)
     def lit: Parser[JSON] =
       "null".as(JNull) |
       double.map(JNumber) |
@@ -27,9 +28,9 @@ object JSON {
       "true".as(JBool(true)) |
       "false".as(JBool(false))
 
-    def value = lit | obj | array
+    def value: Parser[JSON] = lit | obj | array
 
 
-    array | obj
+    obj | array
   }
 }
