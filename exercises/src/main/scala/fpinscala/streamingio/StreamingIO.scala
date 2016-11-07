397e3ce03da9e13c8869fabb953a4aa89870c78c
Tillaert <ardjan@gmail.com>
2016-11-07 23:00:33 +0100

    Exercise 15.5

--- exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -143,7 +143,15 @@ input (`Await`) or signaling termination via `Halt`.
     /*
      * Exercise 5: Implement `|>`. Let the types guide your implementation.
      */
-    def |>[O2](p2: Process[O, O2]): Process[I, O2] = ???
+    def |>[O2](p2: Process[O, O2]): Process[I, O2] = p2 match {
+      case Halt() => Halt()
+      case Emit(h,t) => Emit[I,O2](h, this |> p2)
+      case Await(recv) => this match {
+        case Emit(h,t) => t |> recv(Some(h))
+        case Halt() => Halt() |> recv(None)
+        case Await(recv2) => Await{(i) => recv2(i) |> p2}
+      }
+    }
 
     /*
      * Feed `in` to this `Process`. Uses a tail recursive loop as long
